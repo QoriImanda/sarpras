830ae2248c9976e8d2c1c58fae22cb862b61feaf
Qori Imanda <imandaqori@gmail.com>
2024-06-21 14:04:20 +0700

    update

--- app/Http/Controllers/CetakPDFController.php
+++ app/Http/Controllers/CetakPDFController.php
@@ -8,32 +8,32 @@ class CetakPDFController extends Controller
 {
     public function cetakPdf($tahun, $semester)
     {
-        
 
-        if ($jdwlSidng && $mhsBimbingan && $ketuaDewanPenguji && $sekretaris && $penguji1 && $penguji2 && $koordinatorKTI && $judulTA) {
-            $mpdf = new \Mpdf\Mpdf(['mode' => 'utf-8', 'format' => [216, 330]]);
-            $html = view('cetak-laporan', compact(
-                'jdwlSidng',
-                'mhsBimbingan',
-                'ketuaDewanPenguji',
-                'sekretaris',
-                'penguji1',
-                'penguji2',
-                'koordinatorKTI',
-                'judulTA',
-                'nama_fakultas',
-                'fakultas',
-                'waktuSkrng',
-                'bulan',
-                'tahun',
-                'nomor_surat',
-            ))->render();
 
-            $mpdf->WriteHTML($html);
-            $mpdf->Output();
-        } else {
-            // Handle ketika query tidak mengembalikan hasil yang diharapkan
-            // Misalnya, tampilkan pesan error atau lakukan tindakan lain sesuai kebutuhan aplikasi.
-        }
+        // if ($jdwlSidng && $mhsBimbingan && $ketuaDewanPenguji && $sekretaris && $penguji1 && $penguji2 && $koordinatorKTI && $judulTA) {
+        //     $mpdf = new \Mpdf\Mpdf(['mode' => 'utf-8', 'format' => [216, 330]]);
+        //     $html = view('cetak-laporan', compact(
+        //         'jdwlSidng',
+        //         'mhsBimbingan',
+        //         'ketuaDewanPenguji',
+        //         'sekretaris',
+        //         'penguji1',
+        //         'penguji2',
+        //         'koordinatorKTI',
+        //         'judulTA',
+        //         'nama_fakultas',
+        //         'fakultas',
+        //         'waktuSkrng',
+        //         'bulan',
+        //         'tahun',
+        //         'nomor_surat',
+        //     ))->render();
+
+        //     $mpdf->WriteHTML($html);
+        //     $mpdf->Output();
+        // } else {
+        //     // Handle ketika query tidak mengembalikan hasil yang diharapkan
+        //     // Misalnya, tampilkan pesan error atau lakukan tindakan lain sesuai kebutuhan aplikasi.
+        // }
     }
 }
